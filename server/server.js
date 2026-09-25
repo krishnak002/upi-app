80e52cfc07047c399fdc37f78d320e0d9f48fb73
krishnak002 <krishnakanjani2@gmail.com>
2026-09-25 18:33:44 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -10,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+const clientDistPath = path.resolve(__dirname, "../client/dist");
 
 /*
   GET PAYMENT DETAILS
@@ -28,11 +35,28 @@ app.get("/api/payment-details", (req, res) => {
 */
 
 app.get("/", (req, res) => {
+  if (fs.existsSync(clientDistPath)) {
+    res.sendFile(path.join(clientDistPath, "index.html"));
+    return;
+  }
+
   res.json({
     message: "UPI Payment Server is running",
   });
 });
 
+if (fs.existsSync(clientDistPath)) {
+  app.use(express.static(clientDistPath));
+
+  app.use((req, res, next) => {
+    if (req.path.startsWith("/api/")) {
+      next();
+      return;
+    }
+
+    res.sendFile(path.join(clientDistPath, "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
